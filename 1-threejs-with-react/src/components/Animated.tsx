b5f76ac7ddd209e650f96dafbb04215b5328a06b
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2021-12-21 00:32:14 +0100

    Supense add

--- 1-threejs-with-react/src/components/Animated.tsx
+++ 1-threejs-with-react/src/components/Animated.tsx
@@ -1,16 +1,17 @@
-import React from "react";
-import { Canvas ,Vector3} from "@react-three/fiber";
+import React, { Suspense } from "react";
+import { Canvas, Vector3 } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import {Box} from "./Box";
-
+import { Box } from "./Box";
 
 export default function AnimatedPanel() {
   return (
     <Canvas>
-      <OrbitControls enableZoom={false}/>
+      <OrbitControls enableZoom={false} />
       <ambientLight intensity={1} />
-      <directionalLight intensity={0.3} color={"white"} position={[-2,5,2]} />
-      <Box args={[3,3,3]} rotation={[90,0,60]}/>
+      <directionalLight intensity={0.3} color={"white"} position={[-2, 5, 2]} />
+      <Suspense fallback={null}>
+        <Box args={[3, 3, 3]} rotation={[90, 0, 60]} />
+      </Suspense>
       {/* <Box args={[3,3,3]} color='61ffca' rotation={[90,0,20]}/> */}
     </Canvas>
   );
