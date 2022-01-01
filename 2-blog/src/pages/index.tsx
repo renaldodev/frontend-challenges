aa47231475469bbdd0f96a8cf1868302eace61da
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2022-01-01 16:16:54 +0100

    project-2

---
+++ 2-blog/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+       <div className='bg-red-900'>
+         <h1>Rui</h1>
+       </div>
+  )
+}
+
+export default Home
